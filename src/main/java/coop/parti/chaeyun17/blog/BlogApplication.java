2ea3deab19ff6176594b52c37866c4dae2a57f94
Chaeyun Lee <chaeyunn@iochord.com>
2022-01-10 22:24:22 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/coop/parti/chaeyun17/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package coop.parti.chaeyun17.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coop/parti/chaeyun17/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package coop.parti.chaeyun17.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
